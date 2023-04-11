da1cabba69f6601867143d001521630a4226d447
David Giraldo <david.girald0@hotmail.com>
2023-04-11 11:35:58 -0500

    fix: solve problem googleBucketKey on server

---
+++ src/config/gcp/storage.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import googleBucketKey from "../keys/googleBucketKey.json";             // IMPORTANTE. Para que importe la llave en el dist
+import path from "path";
+
+
+export const  uploadFile = async ( ruta: string, destino: string ) => {
+    const GOOGLE_CLOUD_PROJECT = process.env.PROJECT_NAME                       // Nombre del proyecto en Google Cloud
+    const GOOGLE_CLOUD_BUCKET = process.env.BUCKET_NAME                         // Nombre del Bucket
+    const FILE_NAME = path.join(__dirname, `../../../temp/${ruta}`)             // Nombre del archivo a subir      
+    const DETINATION_FILE = destino                                             // Nombre del archivo a subir
+    const { Storage } = require('@google-cloud/storage');                       // Importar la libreria del storage
+
+    const storage = new Storage({
+        projectID: GOOGLE_CLOUD_PROJECT,
+        keyFilename: path.join(__dirname, "../keys/googleBucketKey.json")
+    });
+    
+    try {
+        await storage.bucket(GOOGLE_CLOUD_BUCKET).upload(FILE_NAME, {
+            destination: DETINATION_FILE,
+        });
+    console.log(`${FILE_NAME} uploaded to ${GOOGLE_CLOUD_BUCKET}`);
+    } catch(error) {
+        console.log(error);
+    }
+}
