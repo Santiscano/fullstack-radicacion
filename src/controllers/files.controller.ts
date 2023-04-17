5c67442b672061d5e446e16d2f7705d9559b6771
David Giraldo <david.girald0@hotmail.com>
2023-04-17 11:53:42 -0500

    refactor: modify files controler

--- src/controllers/files.controller.ts
+++ src/controllers/files.controller.ts
@@ -44,14 +44,21 @@ export const postFile = async (req: Request, res: Response) => {
     const tracking_observation = `INICIO DEL PROCESO DEL ${files_registered} EXITOSO`;
     try {
         if(nullValidator(valores)) {
-            return res.status(500).json({message: "ERROR_MISSING_VALUES"});
+            return res.status(422).json({error: true, message: "MISSING_VALUES"});
         };
-        const [ radValidate ] = await connection.query(`
+        const [ registeredVal ] = await connection.query(`
                 SELECT count(*) AS contador FROM files WHERE files_registered = ?;`,
             [ files_registered ]);
+        const [ filesAccountVal ] = await connection.query(`
+                SELECT count(*) AS contador FROM files WHERE files_account_type = ? AND files_account_type_number = ?;`,
+            [ files_account_type, files_account_type_number ]);
         //@ts-ignore
-        if (radValidate[0].contador !== 0){
-            return res.status(500).json({message: `Radicado: ${ files_registered }, ya se encuentra creado en la base de datos`})
+        if (registeredVal[0].contador !== 0){
+            return res.status(500).json({ error: true, message: `Radicado: ${ files_registered.toUpperCase() }, ya se encuentra creado en la base de datos`})
+        };
+        //@ts-ignore
+        if (filesAccountVal[0].contador !== 0){
+            return res.status(500).json({ error: true, message: `${ files_account_type.toUpperCase() }: ${ files_account_type_number.toUpperCase() }, ya se encuentra creado en la base de datos`})
         };
         await connection.query(`
             INSERT INTO files (idproviders, idsedes, idusers, files_type, files_registered, files_price, files_account_type, files_account_type_number)
@@ -69,69 +76,56 @@ export const postFile = async (req: Request, res: Response) => {
         postTraking(idfiles_states, file[0].idfiles, userSession, tracking_observation);
         // createPDF(files_registered.toUpperCase(), files_account_type.toUpperCase(), files_type.toUpperCase());
         return res.status(200).json({ error: false, tracking: "Cargado exitosamente", file });
-    } catch (err) {
-        console.log(err);
+    } catch (error) {
+        console.log(error);
         return res.status(508).json({ error: true, message:"Error del servidor para guardar los archivos" });
     };
 };
 
 // Editar un archivo
 export const putFile = async ( req:Request, res:Response ) => {
-    const { idfiles, 
-            idproviders, 
-            idusers, 
-            idfiles_states, 
-            files_type, 
-            files_registered, 
-            files_cost_center, 
-            files_code_accounting, 
-            files_code_treasury, 
-            files_price,
-            files_account_type, 
-            files_account_type_number,
-            userSession, tracking_observation } = req.body;
+    const { idfiles, idproviders, idusers, idfiles_states, files_type, files_registered, files_cost_center, files_code_accounting, files_code_treasury, files_price,files_account_type, files_account_type_number,userSession, tracking_observation } = req.body;
     const values = [idfiles, idproviders, idusers, idfiles_states, files_type, files_registered, files_price, files_account_type, files_account_type_number, userSession, tracking_observation]
     try {
         if (nullValidator(values)){
-            return res.status(500).json({message: "ERROR_MISSING_VALUES"});
-        }
+            return res.status(422).json({error: true, message: "MISSING_VALUES"});
+        };
         const [ data ] = await connection.query(`SELECT count(*) AS contador FROM files WHERE idfiles = ? OR files_registered = ?;`,
         [ idfiles, files_registered ]);
         // @ts-ignore
         if ( data[0].contador === 0 ) {
-            return res.status(404).json({ message: `Archivo con el id: ${ idfiles } o radicado: ${ files_registered }, no se encuentra registrado en la base de datos` })
-        } else {
-            await connection.query(`
-                UPDATE files SET
-                    idproviders = ?, 
-                    idusers = ?, 
-                    idfiles_states = ?, 
-                    files_type = ?,
-                    files_cost_center = ?, 
-                    files_code_accounting = ?, 
-                    files_code_treasury = ?, 
-                    files_price = ?,
-                    files_account_type = ?, 
-                    files_account_type_number = ?
-                        WHERE idfiles = ? OR files_registered = ?;`,
-                    [ idproviders,
-                    idusers,
-                    idfiles_states,
-                    files_type.toUpperCase(),
-                    files_cost_center,
-                    files_code_accounting,
-                    files_code_treasury,
-                    files_price, 
-                    files_account_type.toUpperCase(), 
-                    files_account_type_number.toUpperCase(), 
-                    idfiles, files_registered ]);
-            const [ fileUpdated ] = await connection.query(`
-                                    SELECT * FROM files WHERE idfiles = ? OR files_registered = ?;`,
-                                    [ idfiles, files_registered ]);
-            postTraking(idfiles_states, idfiles, userSession, tracking_observation.toUpperCase());
-            //@ts-ignore
-            return res.status(200).json({ error: false, tracking: "Cargado exitosamente", fileUpdated });
+            return res.status(404).json({ error: true, message: `Archivo con el id: ${ idfiles } o radicado: ${ files_registered }, no se encuentra registrado en la base de datos` })
         };
+        await connection.query(`
+            UPDATE files SET
+                idproviders = ?, 
+                idusers = ?, 
+                idfiles_states = ?, 
+                files_type = ?,
+                files_cost_center = ?, 
+                files_code_accounting = ?, 
+                files_code_treasury = ?, 
+                files_price = ?,
+                files_account_type = ?, 
+                files_account_type_number = ?
+                    WHERE idfiles = ? OR files_registered = ?;`,
+                [ idproviders,
+                idusers,
+                idfiles_states,
+                files_type.toUpperCase(),
+                files_cost_center,
+                files_code_accounting,
+                files_code_treasury,
+                files_price, 
+                files_account_type.toUpperCase(), 
+                files_account_type_number.toUpperCase(), 
+                idfiles, files_registered ]);
+        const [ fileUpdated ] = await connection.query(`
+            SELECT * FROM files WHERE idfiles = ? OR files_registered = ?;`,
+            [ idfiles, files_registered ]);
+        postTraking(idfiles_states, idfiles, userSession, tracking_observation.toUpperCase());
+        //@ts-ignore
+        return res.status(200).json({ error: false, tracking: "Cargado exitosamente", fileUpdated });
     } catch (error) {
         console.log(error)
         return res.status(508).json({ error: true, message: "Error del servidor para edutar un archivo" })
@@ -143,11 +137,11 @@ export const deleteFile = async (req:Request, res:Response) => {
     const { api_key, files_registered } = req.body;
     const values = [ files_registered ]
     try {
-        if( api_key != process.env.API_KEY) {
-            return res.status(401).json({ message: "No cuentas con los permisos para eliminar un archivo" })
+        if( api_key !== process.env.API_KEY) {
+            return res.status(401).json({ error: true, message: "No cuentas con los permisos para eliminar un archivo" })
         };
         if (nullValidator(values)) {
-            return res.status(500).json({message: "ERROR_MISSING_VALUES"});
+            return res.status(422).json({error: true, message: "MISSING_VALUES"});
         };
         const [ data ] = await connection.query(`SELECT idfiles FROM files WHERE files_registered = ?;`, files_registered );
         //@ts-ignore
